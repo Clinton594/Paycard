40ba9a9e1ff8d440a9dce781895d80194784b710
Clinton Ugochukwu Onuigbo <32768102+Clinton594@users.noreply.github.com>
2023-03-11 09:58:23 +0100

    Responsiveness and UX fixes

---
+++ src/components/YoutubePlayer.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+export default function YoutubePlayer({ youtubeId }: { youtubeId: string }) {
+  return (
+    <>
+      <div className="container">
+        <div className="about-expert">
+          <div className="rounded-lg">
+            <iframe
+              width="80%"
+              height="450px"
+              className="rounded-lg"
+              src={`https://www.youtube.com/embed/${youtubeId}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+      <ul className="shape-group-16 list-unstyled">
+        <li className="shape shape-1">
+          <img src="/img/others/circle-2.png" alt="circle" />
+        </li>
+        <li className="shape shape-2">
+          <img src="/img/others/bubble-2.png" alt="Line" />
+        </li>
+        <li className="shape shape-3">
+          <img src="/img/others/bubble-1.png" alt="Line" />
+        </li>
+      </ul>
+      <ul className="list-unstyled shape-group-9">
+        <li className="shape shape-1">
+          <img src="/img/others/bubble-12.png" alt="Comments" />
+        </li>
+        <li className="shape shape-2">
+          <img src="/img/others/bubble-16.png" alt="Comments" />
+        </li>
+        <li className="shape shape-3">
+          <img src="/img/others/bubble-13.png" alt="Comments" />
+        </li>
+        <li className="shape shape-4">
+          <img src="/img/others/bubble-14.png" alt="Comments" />
+        </li>
+        <li className="shape shape-5">
+          <img src="/img/others/bubble-16.png" alt="Comments" />
+        </li>
+        <li className="shape shape-6">
+          <img src="/img/others/bubble-15.png" alt="Comments" />
+        </li>
+        <li className="shape shape-7">
+          <img src="/img/others/bubble-16.png" alt="Comments" />
+        </li>
+      </ul>
+    </>
+  );
+}
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -3,10 +3,11 @@ import { useParams } from "react-router";
 import { PulseLoader } from "react-spinners";
 import { toast } from "react-toastify";
 import Footer from "../components/Footer";
-import { Item, Play } from "../components/Icons";
+import { Item } from "../components/Icons";
 import Navbar from "../components/Navbar";
 import Page from "../components/Page";
 import ScreenLoader from "../components/ScreenLoader";
+import YoutubePlayer from "../components/YoutubePlayer";
 import { ILaunch } from "../constants/interface";
 import launch from "../constants/launch";
 import { extractLaunchData, fetchData, get_random, scrollTo } from "../helpers/functions";
@@ -48,45 +49,29 @@ export default function Detail() {
       )}
       {!loading && (
         <>
-          <div className="breadcrum-area breadcrumb-banner single-breadcrumb">
-            <div className="container">
-              <div className="row align-items-center">
-                <div className="col-lg-6">
-                  <div className="section-heading heading-left">
-                    <h1 className="title h2">{content.name}</h1>
-                    <span>
-                      A quick view of {content.name} launch event and the problem solving processes underwent with the
-                      awesome team and crew members on the {date}.
-                    </span>
-                    <button
-                      onClick={() => scrollTo(youtubePlayer)}
-                      type="button"
-                      className="axil-btn btn-fill-primary mt-3"
-                    >
-                      Watch Now
-                    </button>
-                  </div>
-                </div>
-                <div className="col-lg-6">
-                  <div className="banner-thumbnail">
-                    <div className="Illustration">
-                      <img className="zoomable" src={image} alt="Illustration" />
+          <div className="banner banner-style-4" style={{ backgroundImage: `url(${image})` }}>
+            <div className="breadcrum-area breadcrumb-banner">
+              <div className="container">
+                <div className="row align-items-center">
+                  <div className="col-lg-6 offset-lg-1">
+                    <div className="section-heading heading-left">
+                      <h1 className="title h2">{content.name}</h1>
+                      <span>
+                        A quick view of {content.name} launch event and the problem solving processes underwent with the
+                        awesome team and crew members on the {date}.
+                      </span>
+                      <button
+                        onClick={() => scrollTo(youtubePlayer)}
+                        type="button"
+                        className="axil-btn btn-fill-primary mt-3"
+                      >
+                        Watch Now
+                      </button>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <ul className="shape-group-8 list-unstyled">
-              <li className="shape shape-1">
-                <img src="/img/others/bubble-9.png" alt="Bubble" />
-              </li>
-              <li className="shape shape-2">
-                <img src="/img/others/bubble-20.png" alt="Bubble" />
-              </li>
-              <li className="shape shape-3">
-                <img src="/img/others/line-4.png" alt="Line" />
-              </li>
-            </ul>
           </div>
           <section className="section-padding single-portfolio-area">
             <div className="container">
@@ -139,56 +124,7 @@ export default function Detail() {
           </section>
 
           <div className="section call-to-action-area" ref={youtubePlayer}>
-            <div className="container">
-              <div className="about-expert">
-                <div className="rounded-lg">
-                  <iframe
-                    width="100%"
-                    height="550px"
-                    className="rounded-lg"
-                    src={`https://www.youtube.com/embed/${content.webcast}`}
-                    title="YouTube video player"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen
-                  ></iframe>
-                </div>
-              </div>
-            </div>
-            <ul className="shape-group-16 list-unstyled">
-              <li className="shape shape-1">
-                <img src="/img/others/circle-2.png" alt="circle" />
-              </li>
-              <li className="shape shape-2">
-                <img src="/img/others/bubble-2.png" alt="Line" />
-              </li>
-              <li className="shape shape-3">
-                <img src="/img/others/bubble-1.png" alt="Line" />
-              </li>
-            </ul>
-            <ul className="list-unstyled shape-group-9">
-              <li className="shape shape-1">
-                <img src="/img/others/bubble-12.png" alt="Comments" />
-              </li>
-              <li className="shape shape-2">
-                <img src="/img/others/bubble-16.png" alt="Comments" />
-              </li>
-              <li className="shape shape-3">
-                <img src="/img/others/bubble-13.png" alt="Comments" />
-              </li>
-              <li className="shape shape-4">
-                <img src="/img/others/bubble-14.png" alt="Comments" />
-              </li>
-              <li className="shape shape-5">
-                <img src="/img/others/bubble-16.png" alt="Comments" />
-              </li>
-              <li className="shape shape-6">
-                <img src="/img/others/bubble-15.png" alt="Comments" />
-              </li>
-              <li className="shape shape-7">
-                <img src="/img/others/bubble-16.png" alt="Comments" />
-              </li>
-            </ul>
+            <YoutubePlayer youtubeId={content.webcast} />
           </div>
         </>
       )}
